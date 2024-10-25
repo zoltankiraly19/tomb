ceb4afb2b7ad9daa64036b09fb5f73c94fe8944f
Király Zoltán <kiralyzoltan@DE-H3P3353.duna.local>
2024-10-25 09:28:01 +0200

    update.txt

--- app.py
+++ app.py
@@ -2,11 +2,15 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)  # CORS engedélyezése minden domain számára
+CORS(app)
 
 @app.route('/dropdown', methods=['GET'])
 def get_dropdown():
-    options = ["1", "2", "3"]
+    options = [
+        {"label": "Option 1", "value": "1"},
+        {"label": "Option 2", "value": "2"},
+        {"label": "Option 3", "value": "3"}
+    ]
     return jsonify({"options": options})
 
 if __name__ == '__main__':
