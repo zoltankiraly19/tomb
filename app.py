74b73daff0cd904f6d50cc15aba7ced52fcda367
Király Zoltán <kiralyzoltan@DE-H3P3353.duna.local>
2024-10-25 09:14:57 +0200

    update.txt

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # CORS engedélyezése minden domain számára
 
 @app.route('/dropdown', methods=['GET'])
 def get_dropdown():
